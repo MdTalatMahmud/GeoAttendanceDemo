2022cb493aea1981eba2e6ea9953ad9907f5a978
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-09-25 15:57:49 +0600

    update

--- app/src/main/java/com/example/geoattendencedemo/MainActivity.java
+++ app/src/main/java/com/example/geoattendencedemo/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,WorkerActivity.class);
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
                 startActivity(intent);
             }
         });
