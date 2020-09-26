830b55f6cf092d683b5609e7b0814303320ae6b9
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-09-26 19:52:10 +0600

    update

---
+++ app/src/main/java/com/example/geoattendencedemo/SeeAllEmployeesActivity.java
@@ -0,0 +1,14 @@
+package com.example.geoattendencedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeeAllEmployeesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see_all_employees);
+    }
+}
--- app/src/main/java/com/example/geoattendencedemo/SupervisorActivity.java
+++ app/src/main/java/com/example/geoattendencedemo/SupervisorActivity.java
@@ -2,13 +2,37 @@ package com.example.geoattendencedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SupervisorActivity extends AppCompatActivity {
+
+    Button todaysWorkerButton, seeAllEmployeesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_supervisor);
+
+        todaysWorkerButton = findViewById(R.id.TodaysWorkerBtnID);
+        seeAllEmployeesButton = findViewById(R.id.SeeAllEmployeesBtnID);
+
+        todaysWorkerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SupervisorActivity.this, TodaysWorkerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        seeAllEmployeesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SupervisorActivity.this, SeeAllEmployeesActivity.class );
+                startActivity(intent);
+            }
+        });
     }
 }
