025b376dcf62ed191591102f1dd5f2242934f5ab
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-09-27 14:07:29 +0600

    employee list added

--- app/src/main/java/com/example/geoattendencedemo/SeeAllEmployeesActivity.java
+++ app/src/main/java/com/example/geoattendencedemo/SeeAllEmployeesActivity.java
@@ -3,12 +3,40 @@ package com.example.geoattendencedemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 public class SeeAllEmployeesActivity extends AppCompatActivity {
+
+    ListView listView;
+    //ArrayAdapter<String> adapter;
+    private SearchView employeeSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_see_all_employees);
+
+        listView = findViewById(R.id.EmployeeListViewID);
+        employeeSearchView = findViewById(R.id.employeeSearchViewID);
+
+        String[] employeeNames = getResources().getStringArray(R.array.employee_names);
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(SeeAllEmployeesActivity.this, R.layout.all_employee_view, R.id.textViewID, employeeNames);
+        listView.setAdapter(adapter);
+
+        employeeSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
     }
 }
